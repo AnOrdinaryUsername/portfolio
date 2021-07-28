2ab63afd28249c1c28c1775abb4bb94dbae7b0b1
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-07-27 21:55:36 -0700

    style: Construct a responsive hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import { HollowDots } from 'components/BackgroundSvgs';
+import { PrimaryButton, TertiaryButton } from 'components/Buttons';
 import Metadata from 'components/Metadata';
+import Image from 'next/image';
 import styled from 'styled-components';
+import heroImage from '../../public/images/hero-image.png';
 
 export default function Home() {
   return (
@@ -7,6 +11,7 @@ export default function Home() {
       <Metadata />
       <Header>
         <Introduction>
+          <BackgroundDots />
           <Heading>
             <Greeting>Hiya, I&apos;m</Greeting>
             <Name>Kyle Masa</Name>
@@ -14,7 +19,12 @@ export default function Home() {
           <Description>
             A <JobTitle>front-end developer</JobTitle> that loves to make things pretty.
           </Description>
+          <ButtonWrapper>
+            <PrimaryButton>Resume</PrimaryButton>
+            <TertiaryButton>View more projects</TertiaryButton>
+          </ButtonWrapper>
         </Introduction>
+        <Image src={heroImage} alt="" />
       </Header>
     </>
   );
@@ -22,15 +32,42 @@ export default function Home() {
 
 const Header = styled.header`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  padding: 3.2rem;
+  height: 100%;
+
+  @media ${(p) => p.theme.breakpoints.lg} {
+    flex-direction: row;
+  }
 `;
 
 const Introduction = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   font-weight: 700;
+  padding-bottom: 4.8rem;
+
+  @media ${(p) => p.theme.breakpoints.lg} {
+    padding-bottom: 0;
+    padding-right: 3.2rem;
+    max-width: 40rem;
+    width: 100%;
+  }
+`;
+
+const BackgroundDots = styled(HollowDots)`
+  position: absolute;
+  left: -114px;
+  top: -65px;
+  z-index: -1;
+  opacity: var(--hollow-dots-opacity);
+
+  @media ${(p) => p.theme.breakpoints.med} {
+    left: -167px;
+  }
 `;
 
 const Heading = styled.h1`
@@ -41,19 +78,39 @@ const Heading = styled.h1`
 `;
 
 const Greeting = styled.span`
-  font-size: 3.2rem;
+  font-size: 2.8rem;
+
+  @media ${(p) => p.theme.breakpoints.med} {
+    font-size: 3.2rem;
+  }
 `;
 
 const Name = styled.span`
-  font-size: 6.4rem;
+  font-size: 5.6rem;
   color: var(--accent-primary);
+
+  @media ${(p) => p.theme.breakpoints.med} {
+    font-size: 6.4rem;
+  }
 `;
 
 const Description = styled.p`
-  font-size: 2.4rem;
-  padding-top: 3.2rem;
+  font-size: 2rem;
+  padding: 3.2rem 0;
   line-height: 1.4;
-  max-width: 35rem;
+
+  @media ${(p) => p.theme.breakpoints.med} {
+    font-size: 2.4rem;
+  }
+
+  @media ${(p) => p.theme.breakpoints.lg} {
+    max-width: 35rem;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
 `;
 
 const JobTitle = styled.span`
