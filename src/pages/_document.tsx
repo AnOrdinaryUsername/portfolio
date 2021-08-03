d128905a437a175bc402581cbe4a49123a8a1a8e
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-08-02 19:32:13 -0700

    feat: Add ServerStyleSheet to _document
    So #3de0767fe27be43d21fc0cb2fd8884a1f4c5bf2e didn't fix the
    flicker issue, but maybe this will? It uses SSR and injects
    css to html so there's no delay.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,40 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 import { THEME } from '../constants';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
